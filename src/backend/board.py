20e819aa7431bf0b63bc3e2297eded9504f46c0c
Sam <z5588327@ad.unsw.edu.au>
2025-10-22 12:06:48 +1100

    Integrate zobrist hash updates into Board class

--- src/backend/board.py
+++ src/backend/board.py
@@ -1,5 +1,16 @@
 from dataclasses import dataclass
 from typing import Optional, Tuple
+import zobrist
+
+WKING_LONG = 1 << 3
+WKING_SHORT = 1 << 2
+BKING_LONG = 1 << 1
+BKING_SHORT = 1 << 0
+DEFAULT_CASTLING = WKING_LONG | WKING_SHORT | BKING_LONG | BKING_SHORT
+
+EMPTY = 0
+WPAWN, WKNIGHT, WBISHOP, WROOK, WQUEEN, WKING = 1, 2, 3, 4, 5, 6
+BPAWN, BKNIGHT, BBISHOP, BROOK, BQUEEN, BKING = -1, -2, -3, -4, -5, -6
 
 
 @dataclass
@@ -10,11 +21,8 @@ class MoveRecord:
     from_sq: Tuple[int, int] = (0, 0)
     to_sq: Tuple[int, int] = (0, 0)
     en_passant: bool = False
-
-
-EMPTY = 0
-WPAWN, WKNIGHT, WBISHOP, WROOK, WQUEEN, WKING = 1, 2, 3, 4, 5, 6
-BPAWN, BKNIGHT, BBISHOP, BROOK, BQUEEN, BKING = -1, -2, -3, -4, -5, -6
+    castling_rights: int = DEFAULT_CASTLING
+    en_passant_file: None | int = None
 
 
 class Board:
@@ -22,6 +30,9 @@ class Board:
         self.board = self.starting_pos()
         self.wking_pos = (7, 4)
         self.bking_pos = (0, 4)
+        self.castling_rights = DEFAULT_CASTLING
+        self.en_passant_file = None
+        self.hash = zobrist.hash_board(self.board)
 
     def starting_pos(self):
         return [
@@ -37,40 +48,56 @@ class Board:
 
     def apply_move(self, move):
         (fx, fy), (tx, ty) = move
+        # CAPTURED PIECE POSITION
+        (cx, cy) = (tx, ty)
 
         original_piece = self.board[fx][fy]
-
         captured = self.board[tx][ty]
 
         self.board[tx][ty] = original_piece
         self.board[fx][fy] = EMPTY
-
         en_passant = False
+        removed_castle_right = 0
+        old_castling_rights = self.castling_rights
+        old_en_passant_file = self.en_passant_file
 
         if original_piece == WKING:
             self.wking_pos = (tx, ty)
+            removed_castle_right = WKING_LONG | WKING_SHORT
         elif original_piece == BKING:
             self.bking_pos = (tx, ty)
+            removed_castle_right = BKING_LONG | BKING_SHORT
+        elif original_piece == WROOK and fx == 7:
+            removed_castle_right = WKING_LONG if fy == 0 else WKING_SHORT
+        elif original_piece == BROOK and fx == 0:
+            removed_castle_right = BKING_LONG if fy == 0 else BKING_SHORT
 
+        elif abs(original_piece) == WPAWN and abs(tx - fx) == 2:
+            self.en_passant_file = fy
         # HANDLE EN PASSANT
         elif captured == EMPTY and abs(original_piece) == WPAWN:
             if fy != ty:
-                captured = self.board[tx + abs(original_piece)][ty]
-                self.board[tx + abs(original_piece)][ty] = EMPTY
+                cx = tx + abs(original_piece)
+                captured = self.board[cx][cy]
+                self.board[cx][cy] = EMPTY
                 en_passant = True
         # HANDLE CASTLING
         # CHECK IF KING MADE A 2SQR MOVE
         if abs(original_piece) == WKING and abs(fy - ty) == 2:
             # SHORT
             if ty == 6:
-                rook = self.board[fx][7]
+                cy = 7
+                rook = self.board[fx][cy]
                 self.board[fx][5] = rook  # move rook
                 self.board[fx][7] = EMPTY  # empty the sqr
+                self.hash = zobrist.hash_piece(self.hash, rook, (fx, 5))
             # LONG
             elif ty == 2:
-                rook = self.board[fx][0]
+                cy = 0
+                rook = self.board[fx][cy]
                 self.board[fx][3] = rook  # move rook
-                self.board[fx][0] = EMPTY
+                self.board[fx][cy] = EMPTY
+                self.hash = zobrist.hash_piece(self.hash, rook, (fx, 3))
 
         promotion = None
         if original_piece == WPAWN and tx == 0:
@@ -80,6 +107,16 @@ class Board:
             promotion = BQUEEN
             self.board[tx][ty] = BQUEEN
 
+        self.castling_rights &= ~(removed_castle_right)
+        self.update_hashes(
+            original_piece,
+            move,
+            captured,
+            (cx, cy),
+            old_castling_rights,
+            old_en_passant_file,
+        )
+
         return MoveRecord(
             moved_piece=original_piece,
             captured_piece=captured,
@@ -87,14 +124,20 @@ class Board:
             from_sq=(fx, fy),
             to_sq=(tx, ty),
             en_passant=en_passant,
+            castling_rights=old_castling_rights,
+            en_passant_file=old_en_passant_file,
         )
 
     def undo_move(self, move, move_record):
         (fx, fy), (tx, ty) = move
+        (cx, cy) = (tx, ty)
         # piece = self.board[tx][ty]
 
         self.board[fx][fy] = move_record.moved_piece
         self.board[tx][ty] = move_record.captured_piece
+        old_castling_rights = self.castling_rights
+        old_en_passant_file = self.en_passant_file
+        self.castling_rights = move_record.castling_rights
 
         if move_record.moved_piece == WKING:
             self.wking_pos = (fx, fy)
@@ -104,17 +147,49 @@ class Board:
         if abs(move_record.moved_piece) == WKING and abs(fy - ty) == 2:
             # SHORT
             if ty == 6:
-                rook = self.board[fx][5]
+                cy = 5
+                rook = self.board[fx][cy]
                 self.board[fx][7] = rook  # undo rook
-                self.board[fx][5] = EMPTY
+                self.board[fx][cy] = EMPTY
+                self.hash = zobrist.hash_piece(self.hash, rook, (fx, 7))
+
             # LONG
             elif ty == 2:
-                rook = self.board[fx][3]
-                self.board[fx][0] = rook  # nudo rook
-                self.board[fx][3] = EMPTY
+                cy = 3
+                rook = self.board[fx][cy]
+                self.board[fx][0] = rook  # undo rook
+                self.board[fx][cy] = EMPTY
+                self.hash = zobrist.hash_piece(self.hash, rook, (fx, 0))
 
         # ENPASSANT UNDO
         elif move_record.en_passant:
             self.board[tx][ty] = EMPTY
-            rank = tx + abs(move_record.moved_piece)
-            self.board[rank][ty] = move_record.captured_piece
+            cx = tx + abs(move_record.moved_piece)
+            self.board[cx][ty] = move_record.captured_piece
+
+        # RESTORE HASHES
+        self.update_hashes(
+            move_record.moved_piece,
+            move_record.move,
+            move_record.captured_piece,
+            (cx, cy),
+            old_castling_rights,
+            old_en_passant_file,
+        )
+
+    def update_hashes(
+        self,
+        moved_piece,
+        move,
+        captured_piece,
+        captured_position,
+        old_castling_rights,
+        old_en_passant_file,
+    ):
+        self.hash = zobrist.hash_move(self.hash, moved_piece, move)
+        self.hash = zobrist.hash_piece(self.hash, captured_piece, captured_position)
+        self.hash = zobrist.hash_castle(
+            self.hash, old_castling_rights, self.castling_rights
+        )
+        self.hash = zobrist.hash_en_passant(self.hash, old_en_passant_file)
+        self.hash = zobrist.hash_en_passant(self.hash, self.en_passant_file)
